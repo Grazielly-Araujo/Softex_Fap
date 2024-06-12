ba5251c2fc632865bbada4bc3ff736159aedf99a
Grazielly-Araujo <graziellygalvaoaraujo@gmail.com>
2024-06-12 20:14:06 -0300

    Add exercicio de if else

---
+++ exe001.js
@@ -0,0 +1,42 @@
+//var valor = parseInt(prompt("Informe um número: "))
+/* var valor1 = prompt("Informe um número: ");
+
+if(valor1 >= 10){
+    alert("O Valor informado é maior ou igual a 10");
+}
+else {
+    alert("ALERT!!");
+};
+ */
+
+function funcao(){
+    var valor = parseInt(prompt("Informe um número: "))
+
+    if (valor == 1){
+        alert("Janeiro")
+    }else if (valor == 2) {
+        alert("Fevereiro")
+    }else if (valor == 3) {
+        alert("Março")
+    }else if (valor ==4 ) {
+        alert("Abril")
+    }else if (valor == 5 ) {
+        alert("Maio")
+    }else if (valor == 6) {
+        alert("Junho")
+    }else if (valor ==7 ) {
+        alert("Julho")
+    }else if (valor == 8) {
+        alert("Agosto")
+    }else if (valor ==9) {
+        alert("Setembro")
+    }else if (valor== 10) {
+        alert("Outubro")
+    }else if (valor ==11) {
+        alert("Novembro")
+    }else if (valor ==12) {
+        alert("Dezembro")
+    }else{
+        alert( "O Valor informado não corresponde a um mês do ano. ")
+    }
+}
