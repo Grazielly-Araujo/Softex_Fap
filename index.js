2b30eabec7692ca7e65a45dbedce6af64ee46486
Grazielly-Araujo <graziellygalvaoaraujo@gmail.com>
2024-06-12 18:24:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function funcao1() {
+  var num1 = parseInt(prompt("Informe um número: "));
+  var num2 = parseInt(prompt("Informe outro número: "));
+  var soma = num1 + num2;
+  alert(`A soma de ${num1} + ${num2} = ${soma}`);
+}
+
+let nome1 = prompt("Qual o seu nome? ");
+let idade = prompt("Qual a sua idade?  ");
+
+alert(`Olá, ${nome1} sua idade é ${idade}`);
+console.log("Nome: " + nome1);
+console.log("idade: " + idade);
+
+let nome = prompt("Qual o seu nome? ");
+let profissao = prompt("Qual a sua profissão? ");
+alert(`Olá, ${nome}, sua profissão é ${profissao}`);
+console.log("Nome: " + nome);
+console.log("Profissão: " + profissao);
+
+/* var  nota = 10;
+var nota1 = 8;
+var nota2 = 7;
+var nota3 = 10;
+var nota4 = 7;
+
+var soma = (nota + nota1 + nota2+ nota3 + nota4)/5
+
+console.log(soma)
+ */
+
+/* var a = 10
+var b = 5
+var c = 3
+var d = 2
+
+var soma = (a*d)+ b - (c*d) - (a/d) 
+console.log(soma)
+
+ */
