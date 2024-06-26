1035a261ebaab18c93c133cd74e9170aa7a8220b
Mateus Fernando <mateusfss368@gmail.com>
2024-06-26 20:30:25 -0300

    Exercicio de Laço

---
+++ exe004.js
@@ -0,0 +1,32 @@
+console.log("ALERT")
+
+function funcao(){
+    var ola = 1
+    while(ola<=11){
+        document.write("Olá Turma!")
+        document.write("<br>")
+        ola++
+
+    }
+}
+
+function func() {
+    var imp = 100 
+    while(imp > 0){
+        if(imp%2 == 1){
+            console.log(imp)
+        }
+        imp--;
+
+    }
+
+}
+
+
+
+/* FUNÇÃO */
+
+
+
+
+
